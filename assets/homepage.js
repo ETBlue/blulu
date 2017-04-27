a89c547553268b6e7558ddf6dcf0f2a236521533
ETBlue <etblue.tw@gmail.com>
2017-04-27 17:59:39 +0800

    add youtube player api suggested by @bobbytung

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,3 +1,47 @@
-$(function(){
+// async script loading
+var tag = document.createElement('script');
+tag.id = 'youtube_iframe_api';
+tag.src = 'https://www.youtube.com/iframe_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-});
+map = [
+  {
+    id: 'video_alpha1',
+    toc: 'alpha1'
+  },
+  {
+    id: 'video_alpha2',
+    toc: 'alpha2'
+  },
+  {
+    id: 'video_beta',
+    toc: 'beta'
+  },
+  {
+    id: 'video_gamma',
+    toc: 'gamma'
+  },
+];
+
+videos={};
+
+function onYouTubeIframeAPIReady() {
+
+  map.forEach(function(item){
+    videos[item.toc] = new YT.Player(item.id, {
+      events: {
+        'onReady': onPlayerReady(item.toc),
+      }
+    });
+  });
+}
+
+function onPlayerReady(id) {
+  $(function(){
+    $('[data-toc_id="' + id + '"] .item').on('click tap', function () {
+      var timestamp = $(this).data('timestamp');
+      videos[id].playVideo().seekTo(timestamp, true);
+    });
+  });
+}
